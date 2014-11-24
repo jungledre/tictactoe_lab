43a610b8ba9e5a60f1ebe67080c71c3cc1e8dcc1
Adrienne <adrie.k@gmail.com>
2014-11-23 17:39:02 -0800

    MVP

--- main.js
+++ main.js
@@ -2,30 +2,37 @@ document.addEventListener('DOMContentLoaded',function(){
     console.log("hello")
     var gridArray = document.querySelectorAll('.cell')
     var grid = document.querySelector('.cell')
+    var occupied = document.querySelectorAll('.occupied')
     var count = 0
-    var count2 = 0
     var button = document.querySelector('#button')
     var gridLength = gridArray.length
-
+    var i = 0
+    var j = 0
+    var reset = function() {
+        for (var j=0; j < gridArray.length; j++) {
+            console.log(j)
+            gridArray[j].innerText = "?";
+            gridArray[j].classList.remove('occupied')
+            gridArray[j].style.color = "black"
+        }
+        i = 0
+        count = 0
+    }
 
     for (var i=0; i < gridArray.length; i++) {
         gridArray[i].addEventListener('click',function(){
             if (this.classList.contains('occupied') === false) {
                 count ++
-                this.innerText = (count%2 == 1) ? "X":"O";
+                this.innerText = (count%2 == 1) ? "M":"O";
                 this.classList.add('occupied')
                 console.log("hi")
                 this.style.color = (count%2 == 1) ? "navy":"fuchsia";
             }
         });
     }
-    for (var j=0; j < 9; j++) {
-        button.addEventListener("click", function(){
-            console.log(j)
-            gridArray[j].innerText = "M";
-            gridArray[j].classList.remove('occupied')
-            })
-        }
+    button.addEventListener("click", function(){
+        reset()
+    });
 });
 
 
