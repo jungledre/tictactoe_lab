ba42ef981227aa670794ee393e1833904338aa09
Adrienne <adrie.k@gmail.com>
2014-11-24 11:43:29 -0800

    One winning combination.

--- main.js
+++ main.js
@@ -1,46 +1,47 @@
 document.addEventListener('DOMContentLoaded',function(){
-    console.log("hello")
     var gridArray = document.querySelectorAll('.cell')
-    var grid = document.querySelector('.cell')
     var occupied = document.querySelectorAll('.occupied')
     var turn = document.querySelector('#turn')
-    var count = 0
     var button = document.querySelector('#button')
     var gridLength = gridArray.length
-    var i = 0
-    var j = 0
+    var winners = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    var count = 0
+
     var reset = function() {
-        for (var j=0; j < gridArray.length; j++) {
-            console.log(j)
-            gridArray[j].innerText = "";
-            gridArray[j].classList.remove('occupied')
-            gridArray[j].style.color = "black"
+        for (var i=0; i < gridArray.length; i++) {
+            gridArray[i].innerText = "";
+            gridArray[i].classList.remove('occupied')
+            gridArray[i].style.color = "black"
             turn.innerText = "X"
         }
-        i = 0
         count = 0
     }
 
-    for (var i=0; i < gridArray.length; i++) {
-        gridArray[i].addEventListener('click',function(){
-            if (this.classList.contains('occupied') === false) {
-                count ++
-                this.innerText = (count%2 == 1) ? "X":"O";
-                this.classList.add('occupied')
-                this.style.color = (count%2 == 1) ? "#FFAA01":"#FF5500";
-                turn.innerText = (count%2 == 1) ? "O":"X"
-            }
-        });
+    var winning = function() {
+        for (var j=0; j < gridArray.length; j++) {
+            if  (gridArray[winners[j][0]].innerText!=""
+                && gridArray[winners[j][1]].innerText===gridArray[winners[j][0]].innerText
+                && gridArray[winners[j][2]].innerText===gridArray[winners[j][0]].innerText) {
+                turn.innerText = gridArray[winners[j][0]].innerText + " WINS"
+        };
+        return;
     }
-    button.addEventListener("click", function(){
-        reset()
+}
+
+for (var i=0; i < gridArray.length; i++) {
+    gridArray[i].addEventListener('click',function(){
+        if (this.classList.contains('occupied') === false) {
+            count ++
+            this.innerText = (count%2 == 1) ? "X":"O";
+            this.classList.add('occupied')
+            this.style.color = (count%2 == 1) ? "#FFAA01":"#FF5500";
+            turn.innerText = (count%2 == 1) ? "O":"X"
+            turn.style.fontSize = "3em"
+        }
+        winning()
     });
+}
+button.addEventListener("click", function(){
+    reset()
 });
-
-
-// var array = [[0,1,2],[0,1,2],[0,1,2]]
-// if(array[0][0]===x, array[0][0]===x, array[0][0]===x)
-//     (array[0][0]===x, array[0][0]===x, array[0][0]===x)
-//     (array[0][0]===x, array[0][0]===x, array[0][0]===x){
-//     return "You win!"
-// }
+});
